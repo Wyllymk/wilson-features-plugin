3bb2fa3c546d916ec889ac9454af7b56661ebccc
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-02-15 12:15:05 +0300

    Creation

---
+++ wilson-features.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package           WilsonFeatures
+ * @author            Wilson
+ * @copyright         2023 Mbuthia Inc
+ * @license           GPL-2.0-or-later
+ *
+ * Plugin Name:       Wilson Features
+ * Plugin URI:        https://github.com/Wyllymk/wilson-features-plugin
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Wilson
+ * Author URI:        https://wyllymk.github.io/newport/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/Wyllymk/wilson-features-plugin
+ * Text Domain:       wilson-features
+ * Domain Path:       /languages
+ */
