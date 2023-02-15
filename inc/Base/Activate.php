1709593bf653e362f748ebc3f2e351621f6db5ce
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-02-15 19:28:16 +0300

    static

---
+++ inc/Base/Activate.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package           WilsonFeatures
+ */
+
+ class Activate{
+    public static function activate(){
+        //$this->custom_post_type();
+        flush_rewrite_rules();
+    }
+ }
---
+++ uninstall.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package           WilsonFeatures
+ */
+
+/**
+ * Trigger this file on plugin install
+ */
+
+ /*-------------------------------------------------------------------------*/
+/*                        SECURITY CHECK                                   */
+/*-------------------------------------------------------------------------*/
+if(! defined('WP_UNINSTALL_PLUGIN')){
+    die;
+}
+
+ /*-------------------------------------------------------------------------*/
+/*                        CLEAR DATABASE DATA                              */
+/*-------------------------------------------------------------------------*/
+
+//Access database via SQL
+
+global $wpdb;
+$wpdb->query("DELETE FROM wp_posts WHERE post_type = 'book'");
+$wpdb->query("DELETE FROM wp_posts_meta WHERE post_id NOT IN (SELECT ID FROM wp_posts)");
+$wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT ID FROM wp_posts)");
